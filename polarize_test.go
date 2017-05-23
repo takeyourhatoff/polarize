c29ad40ed22013bcdceb2bcd8de2b6218365da13
Paddy Steed <jarktasaa@gmail.com>
2017-05-23 18:05:27 +0100

    Add tests and benchmarks for YCbCr optimimization

--- polarize_test.go
+++ polarize_test.go
@@ -19,6 +19,18 @@ func BenchmarkPolarimetricImage_addSample(b *testing.B) {
 	})
 }
 
+func BenchmarkPolarimetricImage_addYCbCrSample(b *testing.B) {
+	r := image.Rect(0, 0, 1000, 1000)
+	img := newPolarimetricImage(r)
+	sample := image.NewYCbCr(r, image.YCbCrSubsampleRatio420)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			img.addSample(0, sample)
+		}
+	})
+}
+
 func BenchmarkPolarimetricImage_At(b *testing.B) {
 	r := image.Rect(0, 0, 1000, 1000)
 	img := newPolarimetricImage(r)
@@ -40,6 +52,33 @@ func TestPolarimetricImage(t *testing.T) {
 		sample.Set(x, 0, color.White)
 		pol.addSample(x, sample)
 		sample.Set(x, 0, color.Black)
+	}
+	exp := []color.Color{
+		hsv.HSV{H: 0 / 3.0, S: 2 / 3.0, V: 1 / 3.0},
+		hsv.HSV{H: 1 / 3.0, S: 2 / 3.0, V: 1 / 3.0},
+		hsv.HSV{H: 2 / 3.0, S: 2 / 3.0, V: 1 / 3.0},
+	}
+	for x, c := range exp {
+		if colorEq(pol.At(x, 0), c) == false {
+			t.Errorf("x=%v, pol.At(x, 0) = %#v, exp[x] = %#v\n", x, pol.At(x, 0), c)
+		}
+	}
+}
+
+func setYCbCr(i *image.YCbCr, x, y int, c color.Color) {
+	Y := color.GrayModel.Convert(c).(color.Gray).Y
+	j := i.YOffset(x, y)
+	i.Y[j] = Y
+}
+
+func TestPolarimetricYCbCrImage(t *testing.T) {
+	r := image.Rect(0, 0, 3, 1)
+	pol := newPolarimetricImage(r)
+	sample := image.NewYCbCr(r, image.YCbCrSubsampleRatio420)
+	for x := 0; x < 3; x++ {
+		setYCbCr(sample, x, 0, color.White)
+		pol.addSample(x, sample)
+		setYCbCr(sample, x, 0, color.Black)
 	}
 	exp := []color.Color{
 		hsv.HSV{H: 0 / 3.0, S: 2 / 3.0, V: 1 / 3.0},
